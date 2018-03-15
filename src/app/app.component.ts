15d899724d0a9be114699c5ababd8d89481b16bd
jack <antagoria@hotmail.com>
2018-03-14 22:50:49 -0600

    asdasd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import { Globals } from './globals'
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,81 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = '';
+	private role: string;
+
+	constructor(private globals: Globals) {
+		this.role = globals.role;
+	}
+
+	menus: LosSideMenu[] = [
+		{
+			name: 'Home',
+			path: '',
+			icon: 'public',
+			isDropDown: false,
+			childs: [],
+			handle: null
+		},
+		{
+			name: 'About',
+			path: 'about',
+			icon: 'error',
+			isDropDown: false,
+			childs: [],
+			handle: null
+		},
+		{
+			name: 'Protected',
+			path: 'protected',
+			icon: 'lock',
+			isDropDown: false,
+			childs: [],
+			handle: null
+		},
+		{
+			name: 'Function',
+			path: '',
+			icon: 'touch_app',
+			isDropDown: false,
+			childs: [],
+			handle: () => {
+				console.log('Pusalte el botón en el menú');
+			},
+			handleIcon: 'warning'
+		},
+		{
+			name: 'Sub Menu',
+			path: '',
+			icon: 'folder',
+			isDropDown: true,
+			childs: [
+				{
+					name: 'Home',
+					path: '',
+					icon: 'public',
+					isDropDown: false,
+					childs: [],
+					handle: null
+				},
+				{
+					name: 'About',
+					path: 'about',
+					icon: 'error',
+					isDropDown: false,
+					childs: [],
+					handle: null
+				},
+				{
+					name: 'Protected',
+					path: 'protected',
+					icon: 'lock',
+					isDropDown: false,
+					childs: [],
+					handle: null
+				}
+			],
+			handle: null
+		}
+	];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,67 @@
-import { BrowserModule } from '@angular/platform-browser';
+﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
+import { Globals } from './globals'
+
+import { LosRoute } from 'ng-los';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './vistas/login/login';
+import { mainComponent } from './vistas/main/main';
+
+
+import { LosModule } from 'ng-los';
+
+
+
+const appRoutes: Routes = [	
+	{
+		path: 'login',
+		component: LoginComponent,
+		data: { title: 'Login' }
+	},
+	{
+		path: '',
+		redirectTo: '/login',
+		pathMatch: 'full'
+	},
+	{
+		path: 'home',
+		component: mainComponent,
+		data: { title: 'home' }
+	},
+	{ path: '**', component: LoginComponent }
+];
+
+
+const routes: LosRoute[] = [
+	{
+		path: 'login',
+		component: LoginComponent,
+		losAuthActivate: false
+	}
+];
 
 
 @NgModule({
-  declarations: [
-    AppComponent
+	declarations: [
+	  LoginComponent,
+	  AppComponent,
+	  mainComponent
+	  
   ],
   imports: [
-    BrowserModule
+	  BrowserModule,
+	  FormsModule,
+	  LosModule.forRoot(),
+      LosModule.forRoutes(routes),
+      RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+  providers: [Globals],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
