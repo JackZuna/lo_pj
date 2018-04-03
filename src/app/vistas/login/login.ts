842fa893056dced55bb86817022a59723a12534d
jack <antagoria@hotmail.com>
2018-04-02 21:01:56 -0500

    ,

---
+++ src/app/vistas/login/login.ts
@@ -0,0 +1,50 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Globals } from '../../globals'
+import { UtilService } from '../../util.service'
+import { LosAuthService, LosLoginModel } from 'los-auth';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+export class LoginComponent {
+
+	data: LosLoginModel = {
+		username: 'usuario.desarrollo',
+		password: 'pzGq5uixmr25',
+		client_id: 'cc73800ebd6544d793b88a2aec59ca41',
+		url: '',
+		grant_type: 'password'
+	}
+
+	private clickMessage: string;
+	private username: string;
+	private password: string;
+	private role: string;
+
+	constructor(private globals: Globals, private utilService: UtilService
+		, private authService: LosAuthService) {
+		this.role = globals.role;
+	}
+
+	
+
+	private changedRole() {
+		this.utilService.calllogin();
+		
+		this.clickMessage = 'You are my hero!';
+	}
+	private onLogin() {
+		
+		this.authService.login(this.data).subscribe(response => {
+			this.globals.role = response.access_token;
+			this.changedRole();	
+		}, responseError => {
+			this.clickMessage = 'buuuu';
+
+		});
+	}
+}
---
+++ src/app/vistas/main/main.ts
@@ -0,0 +1,54 @@
+﻿import { Component } from '@angular/core';
+import { Globals } from '../../globals'
+import { LosSideMenu } from 'ng-los';
+import { LosModule } from 'ng-los';
+
+
+@Component({
+  selector: 'main',
+  templateUrl: './main.html',
+  styleUrls: ['./app.component.css']
+})
+export class mainComponent {
+    title = '';
+	private role: string;
+
+	constructor(private globals: Globals) {
+		this.role = globals.role;
+	}
+
+	menus: LosSideMenu[] = [
+		{
+			name: 'Home',
+			path: '',
+			icon: 'public',
+			isDropDown: false,
+			childs: [],
+			handle: null
+		},		
+		{
+			name: 'ABC',
+			path: '',
+			icon: 'folder',
+			isDropDown: true,
+			childs: [
+				{
+					name: 'Marca',
+					path: 'marca',
+					icon: 'public',
+					isDropDown: false,
+					childs: [],
+					handle: null
+				}, {
+					name: 'Seccion',
+					path: 'seccion',
+					icon: 'public',
+					isDropDown: false,
+					childs: [],
+					handle: null
+				}
+			],
+			handle: null
+		}
+	];
+}
